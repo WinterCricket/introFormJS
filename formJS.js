d75492916556dfd90301b947c0cec6a88cb0db3b
WinterCricket <hodhodhod2003@gmail.com>
2018-08-26 15:21:03 -0500

    delta order of js

--- formJS.js
+++ formJS.js
@@ -12,17 +12,17 @@ function validateForm() {
 	var funnyName = document.getElementById('funnyName');
 	var theValue = funnyName.value;
 	if(theValue != "Smimey Coats") {
-		console.log("WARNING! You are attempting to breach Winter Cricket security!");
+		alert("WARNING! You are attempting to breach Winter Cricket security!");
 
 		var messageHolder = document.getElementById('target');
 		messageHolder.style.color = "red";
 		messageHolder.innerHTML = "<h2>WARNING: Enter your correct Funny Name!</h2>";
 		funnyName.select();
 		//funnyName.focus();
-
+		alert("BREACH ATTEMPTED!");
 		return false;
 
-		//console.log("BREACH ATTEMPTED!");
+		
 	} 
 
 
